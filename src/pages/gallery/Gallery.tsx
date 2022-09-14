3d06bb374cf229aabff950e6b30a122ba4a3dea2
David Sanchez <fernandoge210895@gmail.com>
2022-09-14 17:11:21 -0500

    page Gallery completed in al devices

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -1,7 +1,40 @@
-const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+import { useEffect, useState } from 'react';
 
-export default Gallery
+import './gallery.scss';
+import { Banner } from '../../UI';
+import Image from '../../images/header_bg_3.jpg';
+
+const Gallery = () => {
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const temp: string[] = [];
+    for (let i = 0; i < 15; i++) {
+      temp.push(require(`../../images/gallery${i + 1}.jpg`));
+    }
+    setImages(temp);
+  }, []);
+
+
+  return (
+    <>
+      <Banner title="Our Gallery" image={Image}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Quibusdam ut natus hic error ?
+      </Banner>
+      <section className="gallery">
+        <div className="gallery__container container">
+          {
+            images.map((image, i) => (
+              <article key={i} className="gallery__image">
+                <img src={image} alt={`Gallery - ${i + 1}`} />
+              </article>
+            ))
+          }
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Gallery;
