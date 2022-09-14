5ee8bf5edc8ba8db96a8e6865e12e807242b6aa6
David Sanchez <fernandoge210895@gmail.com>
2022-09-14 14:11:11 -0500

    page Contact completed in al devices

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,8 +1,50 @@
 import './contact.scss';
+import Image from '../../images/header_bg_2.jpg';
+
+import { MdEmail } from 'react-icons/md';
+import { BsMessenger } from 'react-icons/bs';
+import { IoLogoWhatsapp } from 'react-icons/io';
+
+import './contact.scss';
+import { Banner } from '../../UI';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <Banner title='Get In Touch' image={Image}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ut natus hic error ?
+      </Banner>
+      <section className="contact">
+        <div className="contact__container container">
+          <div className="contact__wrapper">
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              className='contact__link'
+              href="mailto:dsanchezd21@gmail.com"
+            >
+              <MdEmail />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              className='contact__link'
+              href="mailto:dsanchezd21@gmail.com"
+            >
+              <BsMessenger />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              className='contact__link'
+              href="https://wa.me/+51945217181"
+            >
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
